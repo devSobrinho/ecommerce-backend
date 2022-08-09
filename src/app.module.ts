1c4d504ccf2e352d525f4b6cee7f2c1966000170
devSobrinho <dev.sobrinho@gmail.com>
2022-08-09 00:28:40 -0300

    :feat: Implement config app module, main and create prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,31 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { PermissionModule } from './permission/permission.module';
+import { RoleModule } from './role/role.module';
+import { ConfigModule } from '@nestjs/config';
+import config from './common/configs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      context: ({ req }) => ({ req }),
+    }),
+    UserModule,
+    AuthModule,
+    RoleModule,
+    PermissionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
